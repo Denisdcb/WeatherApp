5d78cdd4b319c1c3560cb49770f2108728da38f3
Denisdcb <denis.dcbr@gmail.com>
2024-12-22 16:38:44 +0100

    First update

--- src/app/components/main-display/main-display.component.ts
+++ src/app/components/main-display/main-display.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { CitySearchFormComponent } from '../city-search-form/city-search-form.component';
 import { WeatherDisplayComponent } from '../weather-display/weather-display.component';
 
 @Component({
   selector: 'app-main-display',
   standalone: true,
-  imports: [HeaderComponent, WeatherDisplayComponent],
+  imports: [HeaderComponent, WeatherDisplayComponent, CitySearchFormComponent],
   templateUrl: './main-display.component.html',
   styleUrl: './main-display.component.css'
 })
--- src/app/components/weather-display/weather-display.component.ts
+++ src/app/components/weather-display/weather-display.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import { CitySearchFormComponent } from '../city-search-form/city-search-form.component';
+import { Component, inject, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-weather-display',
   standalone: true,
-  imports: [CitySearchFormComponent],
+  imports: [],
   templateUrl: './weather-display.component.html',
   styleUrl: './weather-display.component.css'
 })
 export class WeatherDisplayComponent {
+  weather: any = [];
+  main: any = [];
+  city: string = '';
+  temp: number = 0;
+  tempFeels: number = 0;
 
+  private weatherService: WeatherService = inject(WeatherService);
+
+  ngOnInit(): void {
+    this.weatherService.getWeather('Montréal').subscribe(data => {
+      this.weather = data.weather[0];
+      this.main = data.main;
+      this.city = data.name;
+      this.temp = Math.round(this.main.temp - 273.15);
+      this.tempFeels = Math.round(this.main.feels_like - 273.15);
+    });
+  }
 }
