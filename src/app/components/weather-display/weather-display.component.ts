df7839e77f527e550e3fda13f3a2bb94821b331f
Denisdcb <denis.dcbr@gmail.com>
2024-12-24 09:55:12 +0100

    Final update

--- src/app/components/weather-display/weather-display.component.ts
+++ src/app/components/weather-display/weather-display.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HeaderComponent } from '../header/header.component';
+
 
 
 @Component({
@@ -16,7 +17,7 @@ export class WeatherDisplayComponent {
   private weatherService: WeatherService = inject(WeatherService);
 
   myForm = new FormGroup({
-      city: new FormControl(''),
+      city: new FormControl('', [Validators.required, Validators.minLength(3)]),
     });
 
   weather: any = [];
@@ -27,12 +28,12 @@ export class WeatherDisplayComponent {
 
   onSubmit() {
     const formData = this.myForm.value.city;
-    this.weatherService.getWeather(this.myForm.value.city).subscribe(data => {
-      this.weather = data.weather[0];
-      this.main = data.main;
+    this.weatherService.getWeather(formData!).subscribe(data => {
       this.city = data.name;
-      this.temp = Math.round(this.main.temp - 273.15);
-      this.tempFeels = Math.round(this.main.feels_like - 273.15);
+      this.main = data.main;
+      this.weather = data.weather[0];
+      this.temp = Math.round(data.main.temp - 273.15);
+      this.tempFeels = Math.round(data.main.feels_like - 273.15);
     });
   }
 
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,29 +1,18 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { map, Observable, tap, switchMap } from 'rxjs';
+import { API_KEYSECRET } from './API_KEY';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
   http: HttpClient = inject(HttpClient);
-  private API_KEY: string = '89a88172e7af87a32c519850ef7d01fb'
-  private geoUrl: string = 'http://api.openweathermap.org/geo/1.0/direct?q=';
-  private weatherUrl: string = 'https://api.openweathermap.org/data/2.5/weather?lat=';
+  private API_KEY: string = API_KEYSECRET;
+  private weatherUrl: string = 'https://api.openweathermap.org/data/2.5/weather?q=';
 
-  getWeather(city: any): Observable<any> {
-    return this.http.get<any>(this.geoUrl + city + '&appid=' + this.API_KEY).pipe(
-      tap(data => {
-        if (data.length === 0) {
-          throw new Error('Aucune coordonnée trouvée pour cette ville.');
-        }
-      }),
-      map(data => data[0]),
-      switchMap(coord =>
-        this.http.get<any>(
-          `${this.weatherUrl}${coord.lat}&lon=${coord.lon}&appid=${this.API_KEY}`
-        )
-      )
-    );
+  getWeather(city: string): Observable<any> {
+    return this.http.get<any>(this.weatherUrl + city + '&appid=' + this.API_KEY)
   }
 }
